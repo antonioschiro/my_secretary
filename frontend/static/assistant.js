20058369066db14340069e43777aa296b7f7a56a
antonioschiro <antonioschiro21@gmail.com>
2025-07-14 15:00:50 +0200

    Enable/Disable form feature implemented. CSS added.

--- frontend/static/assistant.js
+++ frontend/static/assistant.js
@@ -1,5 +1,9 @@
+let input = document.getElementById("messageText")
+let button = document.getElementById("sendButton")
+let messages = document.getElementById('messages')
+let ws = new WebSocket("ws://localhost:8000/ws")
+
 const appendMessage = (messageContent, side) => {
-    let messages = document.getElementById('messages')
     let message = document.createElement('div')
     message.classList.add("message-type", side)
     let content = document.createTextNode(messageContent)
@@ -7,16 +11,29 @@ const appendMessage = (messageContent, side) => {
     messages.appendChild(message)
 }
 
-let ws = new WebSocket("ws://localhost:8000/ws")
+const manageToggleForm = (isDisabled) => {
+    let formElements = [input, button]
+    if (isDisabled){
+        for (element of formElements){
+                element.setAttribute("disabled", "true")
+            }
+    }
+    else {
+        for (element of formElements){
+            element.removeAttribute("disabled", "false")
+        }
+    }
+}
 
 ws.onmessage = (event) => {
     appendMessage(event.data, "left")
+    manageToggleForm(false)
 }
 
 const sendMessage = (event) => {
-    let input = document.getElementById("messageText")
     ws.send(input.value)
     appendMessage(input.value, "right")
     input.value = ''
+    manageToggleForm(true)
     event.preventDefault()
 }
